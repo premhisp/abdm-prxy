1b70d8693d3b108f6d0a5e19bacfdf908bcad768
premhisp <prem.kumar@hispindia.org>
2024-05-22 13:17:15 +0530

    added vercel.json

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const corsOptions = {
 app.use(cors())
 app.use('/himsprovider/*', route)
 
-app.use('/', (req, res) => {
-    res.status(200).json({
-        msg: 'connected to our server successfully!'
-    })
-})
+// app.use('/', (req, res) => {
+//     res.status(200).json({
+//         msg: 'connected to our server successfully!'
+//     })
+// })
 app.use((req, res, next) => {
     const error = new Error('Not found.')
     error.status = 404
